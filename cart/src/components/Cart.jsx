a4216da567a3762e929bcab06814c6a5d70e829c
Ashwin <ashwinpalaniappanr@gmail.com>
2025-02-08 03:14:44 -0500

    context wrap and outsource into app

--- cart/src/components/Cart.jsx
+++ cart/src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
-export default function Cart({ onUpdateItemQuantity }) {
-  const { items } = useContext(CartContext);
+export default function Cart() {
+  const { items, updateItemQuantity } = useContext(CartContext);
 
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -17,7 +17,6 @@ export default function Cart({ onUpdateItemQuantity }) {
         <ul id='cart-items'>
           {items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
-
             return (
               <li key={item.id}>
                 <div>
@@ -25,11 +24,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className='cart-item-actions'>
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
